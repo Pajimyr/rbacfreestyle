5d7f0b630ccbe5a31a07655c1bb4ffed081b9ccc
Pajimyr <pajimyrnevim@gmail.com>
2026-06-18 18:10:50 +0200

    deleted duplicate, types refactor

--- types.ts
+++ types.ts
@@ -2,21 +2,5 @@ export type User = {
   id: number;
   userName: string;
   pass: string;
-  role: Roles["admin"] | Roles["user"];
-};
-
-export type Roles = {
-  admin: {
-    permissions: Actions;
-  };
-  user: {
-    permissions: Actions;
-  };
-};
-
-export type Actions = {
-  create: boolean;
-  delete: boolean;
-  read: boolean;
-  update: boolean;
+  role_id: number;
 };
